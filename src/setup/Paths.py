4c71049e63094035511e353ad91c140885cb02ed
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2023-10-13 17:05:44 -0300

    chore(setup): adicionados caminhos padrão do repo

--- src/setup/Paths.py
+++ src/setup/Paths.py
@@ -1,2 +1,6 @@
 class Paths:
-    pass
+    RAW = '/dataset_eeg_cafe2022/raw'
+    RENAMED = '/dataset_eeg_cafe2022/renamed'
+    FORMATTED = '/dataset_eeg_cafe2022/formatted'
+    TRUNCATED = '/dataset_eeg_cafe2022/truncated'
+
