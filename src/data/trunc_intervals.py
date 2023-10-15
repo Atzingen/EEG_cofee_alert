2c97c85231d2de4bd9eef1bbc003d2cdf81d0560
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2023-10-15 11:13:51 -0300

    refactor(trunc_intervals): adequação nos nomes das variáveis da classe

--- src/data/trunc_intervals.py
+++ src/data/trunc_intervals.py
@@ -3,36 +3,39 @@ from typing import List, Dict
 
 
 class TruncIntervals:
-    def __init__(self, chop_files_path: str):
+    def __init__(self, trunc_intervals_path: str):
         self.channels = ['Fp1', 'Fp2', 'C3', 'C4', 'P7', 'P8', 'O1', 'O2']
-        self.chop_files_path = chop_files_path
+        self.trunc_intervals_path = trunc_intervals_path
         
         self.filename = None
-        self.cutting_intervals = None
+        self.file_intervals = None
 
         
     def load_cutting_intervals(self, csv_file):
         self.filename = csv_file.replace('.csv','')
-        self.cutting_intervals = {channel: [] for channel in self.channels}
+        self.file_intervals = {channel: [] for channel in self.channels}
+        
         try:
-            with open(f'{self.chop_files_path}/chop_{self.filename}.json', 'r') as file:
-                self.cutting_intervals = json.load(file)
+            with open(f'{self.trunc_intervals_path}/chop_{self.filename}.json', 'r') as file:
+                self.file_intervals = json.load(file)
         except FileNotFoundError:
-                self.cutting_intervals = {channel: [] for channel in self.channels}
+                self.file_intervals = {channel: [] for channel in self.channels}
 
 
     def save_cutting_intervals(self):
-            with open(f'{self.chop_files_path}/chop_{self.filename}.json', 'w') as file:
-                json.dump(self.cutting_intervals, file)
+            with open(f'{self.trunc_intervals_path}/chop_{self.filename}.json', 'w') as file:
+                json.dump(self.file_intervals, file)
 
                 
     def add_interval(self, channel: str, start: float, end: float):
-        self.cutting_intervals[channel].append({'start': start, 'end': end})
+        self.file_intervals[channel].append({'start': start, 'end': end})
 
 
     def pop_interval(self, channel: str):
-        self.cutting_intervals[channel].pop()
+        self.file_intervals[channel].pop()
 
         
     def get_cutting_intervals(self, channel: str) -> List[Dict[str,float]]:
-        return self.cutting_intervals[channel]
+        return self.file_intervals[channel]
+
+
