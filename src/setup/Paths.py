0208690cc4e8122056a685baba0e3ed6d806f9d8
FilhoDeEd <102630043+FilhoDeEd@users.noreply.github.com>
2023-10-15 15:44:49 -0300

    feat(Paths): implementação do método que retorna o realpath de um recurso
    closes #19

--- src/setup/Paths.py
+++ src/setup/Paths.py
@@ -1,6 +1,24 @@
-class Paths:
-    RAW = '/dataset_eeg_cafe2022/raw'
-    RENAMED = '/dataset_eeg_cafe2022/renamed'
-    FORMATTED = '/dataset_eeg_cafe2022/formatted'
-    TRUNCATED = '/dataset_eeg_cafe2022/truncated'
+from os.path import dirname, join, realpath
+from enum import Enum
 
+
+class Paths:
+    class Resources(Enum):
+        RAW = 0
+        RENAMED = 1
+        FORMATTED = 2
+        TRUNCATED = 3
+
+    __resource_paths__ = {
+        0: 'dataset_eeg_cafe2022\\raw',
+        1: 'dataset_eeg_cafe2022\\renamed',
+        2: 'dataset_eeg_cafe2022\\formatted',
+        3: 'dataset_eeg_cafe2022\\truncated'
+    }
+
+
+    @staticmethod
+    def get_resource_path(resource: Resources) -> str:
+        file_directory = dirname(__file__)
+        project_directory = realpath(join(file_directory, '..', '..'))
+        return join(project_directory, Paths.__resource_paths__[resource.value])
