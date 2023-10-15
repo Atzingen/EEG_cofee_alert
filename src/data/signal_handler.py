a66587dd00c762cee350c22c9e6ed6d43926a9fa
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2023-10-15 12:11:24 -0300

    refactor(signal_handler): adequação na classe Truncate
    - uso das funções certas de TruncIntervals
    
    - renomeadas variáveis

--- src/data/signal_handler.py
+++ src/data/signal_handler.py
@@ -13,33 +13,33 @@ from src.data.trunc_intervals import TruncIntervals
     acordo com um arquivo extra de timestamps.
 '''
 class Truncate:
-    def __init__(self, files_path: str, chop_files_path):
+    def __init__(self, files_path: str, trunc_intervals_path):
         self.files_path = files_path
-        self.chop_files_path = chop_files_path
         self.channels = ['Fp1', 'Fp2', 'C3', 'C4', 'P7', 'P8', 'O1', 'O2']
 
         self.df = None
-        self.intervals = CutIntervals(self.chop_files_path)
-        self.csv_file = None
+        self.trunc_intervals = TruncIntervals(trunc_intervals_path)
+        self.csv_filename = None
 
 
-    def set_chopper(self, csv_file: str):
-        self.csv_file = csv_file
-        self.df = pd.read_csv(f'{self.files_path}/{csv_file}', delimiter=',')
-        self.intervals.load_cutting_intervals(csv_file)
+    def setup_by_filename(self, csv_filename: str):
+        self.csv_filename = csv_filename
+        self.df = pd.read_csv(f'{self.files_path}/{csv_filename}', delimiter=',')
+        self.trunc_intervals.load_file_intervals(csv_filename)
 
 
-    def perform_chop(self):
+    def truncate(self):
         for channel in self.channels:
-            for interval in self.intervals.get_cutting_intervals(channel):
-                self.__chop_signal__(interval['start'], interval['end'], channel)
+            for interval in self.trunc_intervals.get_channel_intervals(channel):
+                self.__replace_with_nan(interval['start'], interval['end'], channel)
 
 
+    # remove this, use FileManager
     def save_data(self, save_path: str):
-        self.df.to_csv(f'{save_path}/{self.csv_file}')
+        self.df.to_csv(f'{save_path}/{self.csv_filename}')
 
 
-    def __chop_signal__(self, start, end, channel_name):
+    def __replace_with_nan(self, start, end, channel_name):
         self.df.loc[(self.df['Timestamp'] >= start) & (self.df['Timestamp'] <= end), channel_name] = np.nan
 
 
