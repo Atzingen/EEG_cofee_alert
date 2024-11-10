13c11627948ea11dd7af786825082992a5cbc507
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-11-10 16:37:23 -0300

    finished rename_raw_files from FileManager
    closes #16

--- src/file/FileManager.py
+++ src/file/FileManager.py
@@ -1,36 +1,55 @@
 import os 
+import shutil
+
+from src.setup.Paths import Paths
+
 
 class FileManager:
-    @classmethod
-    def rename_raw_files(cls):
-        signal_types = ["alpha","chimp","seq","react"]
-        exp_numbers = list(range(1,22))
+    @staticmethod
+    def __create_path_if_not_exists(path: str) -> None:
+        if not os.path.exists(path):
+            os.mkdir(path)
 
+    @staticmethod
+    def rename_raw_files():
+        raw = Paths.get_resource_path(Paths.Resources.RAW)
+        renamed = Paths.get_resource_path(Paths.Resources.RENAMED)
+
+        signal_types = ["alpha", "chimp", "seq", "react"]
+        exp_numbers = list(range(1, 22))
+        coffee_signal_numbers = list(range(1, 3))
+
+        FileManager.__create_path_if_not_exists(renamed)
 
         for sig_t in signal_types:
             for exp in exp_numbers:
                 try:
-                    os.rename(f"main-session_{sig_t}-{exp}_formatted.csv",f"{sig_t}_{exp}.csv")
-                except OSError:
+                    shutil.copyfile(
+                        f"{raw}/main-session_{sig_t}-{exp}_formatted.csv",
+                        f"{renamed}/{sig_t}_{exp}.csv"
+                    )
+                except Exception as e:
+                    print(e)
                     continue
 
-        for coffee_signal_number in range(1,3):
+        for cff_sn in coffee_signal_numbers:
             for exp in exp_numbers:
                 try:
-                    os.rename(f"main-session_cafe-{exp}_{coffee_signal_number}_formatted.csv",f"cafe-{coffee_signal_number}_{exp}.csv")
-                except OSError:
+                    shutil.copyfile(
+                        f"{raw}/main-session_cafe-{exp}_{cff_sn}_formatted.csv",
+                        f"{renamed}/cafe-{cff_sn}_{exp}.csv"
+                    )
+                except Exception as e:
+                    print(e)
                     continue
 
-
     @staticmethod
-    def write_csvs_in_path(path, data_frames, filenames):
+    def write_csvs_in(path, data_frames, filenames):
         if len(data_frames) == 0:
             raise Exception('[ERROR] Cannot write files by empty list!')
 
-        if not os.path.exists(path): 
-            os.mkdir(path)
+        FileManager.__create_path_if_not_exists(path)
 
         for index, df in enumerate(data_frames):
             df.to_csv(f'{path}/{filenames[index]}')
-
 
