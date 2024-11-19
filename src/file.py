7cef2bab36fd53cfd08ad1c4355736f6c335988c
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-11-19 20:42:58 -0300

    extracted variables to class scope in File

--- src/file.py
+++ src/file.py
@@ -8,7 +8,6 @@ from typing import List
 class File:
     __source_path = Path(__file__).parent.resolve()
     __project_path = Path(__source_path).parent.resolve()
-
     __resource_paths = {
         "raw": "dataset_eeg_cafe2022/raw",
         "renamed": "dataset_eeg_cafe2022/renamed",
@@ -16,48 +15,14 @@ class File:
         "truncated": "dataset_eeg_cafe2022/truncated",
         "truncation_intervals": "assets/truncation_intervals"
     }
+    __signal_types = ["alpha", "cafe", "chimp", "seq", "react"]
+    __coffee_type_numbers = list(range(1, 3))
+    __experiment_numbers = list(range(1, 22))
 
     @staticmethod
     def __create_path_if_not_exists(path: str) -> None:
         if not os.path.exists(path):
             os.mkdir(path)
-
-    @classmethod
-    def get_path_by(cls, resource_name: str) -> Path:
-        return Path(cls.__project_path, cls.__resource_paths[resource_name])
-
-    @classmethod
-    def rename_raw_files(cls):
-        raw = cls.__resource_paths["raw"]
-        renamed = cls.__resource_paths["renamed"]
-
-        signal_types = ["alpha", "chimp", "seq", "react"]
-        exp_numbers = list(range(1, 22))
-        coffee_signal_numbers = list(range(1, 3))
-
-        cls.__create_path_if_not_exists(renamed)
-
-        for sig_t in signal_types:
-            for exp in exp_numbers:
-                try:
-                    shutil.copyfile(
-                        f"{raw}/main-session_{sig_t}-{exp}_formatted.csv",
-                        f"{renamed}/{sig_t}_{exp}.csv"
-                    )
-                except Exception as e:
-                    print(e)
-                    continue
-
-        for cff_sn in coffee_signal_numbers:
-            for exp in exp_numbers:
-                try:
-                    shutil.copyfile(
-                        f"{raw}/main-session_cafe-{exp}_{cff_sn}_formatted.csv",
-                        f"{renamed}/cafe-{cff_sn}_{exp}.csv"
-                    )
-                except Exception as e:
-                    print(e)
-                    continue
 
     @staticmethod
     def write_dataframes_in(path: str,
@@ -78,4 +43,45 @@ class File:
             gsutil cp -r gs://dataset_eeg_cafe2022 ./
         '''
         pass
+
+    @classmethod
+    def get_files_from(cls, resource: str) -> List[str]:
+        return [file for file \
+                in os.listdir(cls.get_path_by(resource_name=resource))]
+
+    @classmethod
+    def get_path_by(cls, resource_name: str) -> Path:
+        return Path(cls.__project_path, cls.__resource_paths[resource_name])
+
+    @classmethod
+    def rename_raw_files(cls):
+        raw = cls.__resource_paths["raw"]
+        renamed = cls.__resource_paths["renamed"]
+
+        cls.__create_path_if_not_exists(renamed)
+
+        for sig_t in cls.__signal_types:
+            if sig_t == "cafe":
+                continue
+
+            for exp in cls.__experiment_numbers:
+                try:
+                    shutil.copyfile(
+                        f"{raw}/main-session_{sig_t}-{exp}_formatted.csv",
+                        f"{renamed}/{sig_t}_{exp}.csv"
+                    )
+                except Exception as e:
+                    print(e)
+                    continue
+
+        for cff_t in cls.__coffee_type_numbers:
+            for exp in cls.__experiment_numbers:
+                try:
+                    shutil.copyfile(
+                        f"{raw}/main-session_cafe-{exp}_{cff_t}_formatted.csv",
+                        f"{renamed}/cafe-{cff_t}_{exp}.csv"
+                    )
+                except Exception as e:
+                    print(e)
+                    continue
 
